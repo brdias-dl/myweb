1954a1ab723b425ea55b85fbdac7c6db48e68d95
brdias-dl <2190974@my.ipleiria.pt>
2019-12-24 00:24:23 +0000

    Website

--- js/index.js
+++ js/index.js
@@ -1,6 +1,71 @@
 window.onload = () => {
     var aulasArray = [];
 
+    $("#btn-load").click((event) => {
+        $.getJSON('aulas.json', function (jd) {
+
+            aula = [];
+            elemento = document.createElement("div");
+
+            //Criar Nome da Aula
+            elemento_nomeDaAula = document.createElement("p");
+            elemento_nomeDaAula.appendChild(document.createTextNode(jd));
+            elemento.appendChild(elemento_nomeDaAula);
+
+
+
+            //Criar Nome do Professor
+            elemento_nomeDoProfessor = document.createElement("p");
+            elemento_nomeDoProfessor.appendChild(document.createTextNode(nomeDoProfessor.value));
+            elemento.appendChild(elemento_nomeDoProfessor);
+
+
+
+            //Criar Tipo de Aula
+            elemento_opcao = document.createElement("p");
+            if (opcaoPL.checked) {
+                elemento_opcao.appendChild(document.createTextNode("Tipo de aula: (PL)"));
+            } else {
+                elemento_opcao.appendChild(document.createTextNode("Tipo de aula: (TP)"));
+            }
+            elemento.appendChild(elemento_opcao);
+
+
+
+            //Criar Semana
+            elemento_semana = document.createElement("p");
+            elemento_semana.appendChild(document.createTextNode(numeroSemana1.value + "|" + numeroSemana2.value));
+            elemento.appendChild(elemento_semana);
+
+
+
+            //Criar Local
+            elemento_local = document.createElement("p");
+            elemento_local.appendChild(document.createTextNode(nomeEdificio.value + "." + nomeSala.value));
+            elemento.appendChild(elemento_local);
+
+
+
+            //Criar Dia da Semana
+            elemento_diaSemana = document.createElement("p");
+            elemento_diaSemana.appendChild(document.createTextNode(diaSemana.value));
+            elemento.appendChild(elemento_diaSemana);
+
+
+
+            //Criar Hora
+            elemento_hora = document.createElement("p");
+            elemento_hora.appendChild(document.createTextNode(numeroHora1.value + "|" + numeroHora2.value));
+            elemento.appendChild(elemento_hora);
+
+
+
+            aulasArray.push(elemento);
+            document.getElementById("aulas").appendChild(elemento);
+
+        });
+    });
+
     //Obter os elementos
     var nomeDaAula = document.getElementById("escolha-aula");
     var nomeDoProfessor = document.getElementById("escolha-professores");
@@ -18,7 +83,7 @@ window.onload = () => {
 
     function addAula() {
 
-        aula = [];
+        var aula;
         elemento = document.createElement("div");
 
         //Criar Nome da Aula
@@ -76,8 +141,6 @@ window.onload = () => {
 
         aulasArray.push(elemento);
         document.getElementById("aulas").appendChild(elemento);
-        document.getElementById("GenerateJSON").innerText = JSON.stringify(aulasArray);
-        console.log(aulasArray);
 
         $.ajax({
             type: "POST",
@@ -91,7 +154,7 @@ window.onload = () => {
                 console.log("Saved!");
             },
             failure: function () {
-                console.log("Error!");
+                console.log("Dammit!");
             }
         });
     }
